be2e33a5000b665ebade0f49565649a2d798f352
Youssef Rahmouni <youssefrahmouni1p@gmail.com>
2025-08-12 05:27:00 +0100

    Update try.py

--- test/try.py
+++ test/try.py
@@ -7,34 +7,3 @@ def sum(number):
 keyandvalue = {sum(i): i for i in urls}
 for x in keyandvalue:
     print(f'Key:{x.result()} | Value:{keyandvalue[x]}')
-
-''' 
-###############
-
-from concurrent.futures import ThreadPoolExecutor, as_completed
-
-urls = [1,2,3,4]
-
-def sum(number):
-    number = number + 1
-    return number
-
-with ThreadPoolExecutor() as executor:
-    keyandvalue = {executor.submit(sum, i): i for i in urls}
-    for future in as_completed(keyandvalue):
-        print(f'Key: {future.result()} | Value: {keyandvalue[future]} ')
-    
-###############
-from concurrent.futures import ThreadPoolExecutor, as_completed
-
-urls = [1, 2, 3, 4]
-
-def sum(number):
-    number = number + 1
-    return number
-
-with ThreadPoolExecutor() as executor:
-    futures = {executor.submit(sum, i): i for i in urls}
-    for future in as_completed(futures):
-        print(f"Input: {futures[future]}, Output: {future.result()}")
-   ''' 
